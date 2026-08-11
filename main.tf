7b9347b09876c7304f7ab7d8ba8308fa866d0b8c
balkishanece1992-cell <balkishan.ece1992@gmail.com>
2026-08-11 23:34:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,1 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "5.0.1"
-    }
-  }
-}
 
-provider "azurerm" {
-  features {
-    
-  }
-}
-
-resource "azurerm_resource_group" "rg" {
-    name = "bala_rg"
-  location = "eastus"
-}
