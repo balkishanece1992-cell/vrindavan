6f17a85aadfeaad931e45e484aa5c55080e3b42c
balkishanece1992-cell <balkishanece1992@gmail.com>
2026-08-11 23:14:40 +0530

    rg+vnet

---
+++ mainn.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "5.0.1"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {
+    
+  }
+}
+
+resource "azurerm_resource_group" "rg" {
+    name = "mala_rg"
+  location = "eastus"
+}
+resource "azurerm_virtual_network" "vnet" {
+  depends_on = [ azurerm_resource_group.rg ]
+  name = "bala_vnet"
+  location = "eastus"
+  resource_group_name = "mala_rg"
+  address_space = [ "10.0.0.1/16" ]
+}
